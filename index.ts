3687a4e41d16c9535269d9108ee92dbcb9cba3c0
Vtchoo <vittor-limma@hotmail.com>
2021-08-12 12:35:58 -0300

    Save connected ips to database

--- index.ts
+++ index.ts
@@ -1,14 +1,39 @@
+require('dotenv').config()
+
 import express from 'express'
+import connection from './src/connection/database'
+
+import posts from './src/routes/posts'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     console.log(`New connection from ${req.ip}`)
+
+    const result = await connection.query(
+        `INSERT INTO connections (ip, connect_time) VALUES ?`,
+        [[[req.ip, new Date()]]]
+    )
+
+    // console.log(result)
+
     res.json({ message: 'Hello World' })
 })
 
+app.get('/connections', async (req, res) => {
+
+    const [results]: any[][] = await connection.query(`SELECT * FROM connections`)
+
+    // console.log(results)
+
+    res.json({ results, count: results.length })
+})
+
+app.use('/posts', posts)
+
 app.listen(3000, () => {
     console.log('server online')
+    console.log(process.env.TZ)
 })
